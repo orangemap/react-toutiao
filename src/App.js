fabbf474742edfa227e3a5f675f17e632cc7461c
alibaba <wangjianwen777@163.com>
2019-08-28 12:09:12 +0800

    swiper-title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import HeaderBar from './components/HeaderBar'
 import FooterBar from "./components/FooterBar"
+import Home from "./views/Home/title"
 import './utils/iconfont'
 const mainStyles = {
     width: '100%',
-    height: 'calc(100% - 1rem)'
+    height: 'calc(100% - 100px)'
 }
 function App() {
     return (
         <div className="App">
             <HeaderBar></HeaderBar>
             <main style={mainStyles}>
+                <Home></Home>
                 {/* {this.props.children} */}
             </main>
             <FooterBar></FooterBar>
