8ea7b1cc3aca2b1e980b6caad56aae35224d4686
xnwangjianwen3 <wangjianwen777@163.com>
2021-02-08 17:47:59 +0800

    登录请求的封装

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import {Redirect} from "react-router-dom";
 
 class admin extends Component {
     render() {
+        const   user = localStorage.getItem("user")
+        if(!user){
+           return <Redirect to="/login"/>
+        }
         return (
             <div>
-                admin
+                hello,{user}
             </div>
         );
     }
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, message } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import logo from "../../assets/images/logo.png";
 import "./login.less";
 class login extends Component {
   onFinish = (values) => {
-    console.log("Received values of form: ", values);
+    const { username, password } = values;
+    if (username && password) {
+      localStorage.setItem("user", "王建文");
+      message.success("登录成功");
+      this.props.history.push("/admin");
+    }
   };
-  handleConfirmSkuId = (rule, value, callback) => {
-
+  validatorPwd = (rule, value, callback) => {
+    value = value.trim();
+    if (!value) {
+      return Promise.reject("密码必须输入");
+    } else if (value.length < 4) {
+      return Promise.reject("密码必须不能小于4位");
+    } else if (value.length > 12) {
+      return Promise.reject("密码必须不能小于12位");
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+      return Promise.reject("密码必须是英文、字母、下划线");
+    } else {
+      return Promise.resolve();
+    }
   };
   render() {
     return (
@@ -33,16 +49,21 @@ class login extends Component {
                 rules={[
                   {
                     required: true,
+                    whitespace: true,
                     message: "用户名必须输入",
                   },
                   {
-                    min:4,
-                    message: '密码不能少于4个字符',
+                    min: 4,
+                    message: "密码不能少于4个字符",
                   },
-                //   {
-                //     validator: (rule, value, callback) =>
-                //       this.handleConfirmSkuId(rule, value, callback),
-                //   },
+                  {
+                    max: 12,
+                    message: "密码不能少于12个字符",
+                  },
+                  {
+                    pattern: /^[a-zA-Z0-9_]+$/,
+                    message: "密码必须是英文、字母、下划线",
+                  },
                 ]}
               >
                 <Input
@@ -59,8 +80,7 @@ class login extends Component {
                 name="password"
                 rules={[
                   {
-                    required: true,
-                    message: "密码必须输入",
+                    validator: this.validatorPwd,
                   },
                 ]}
               >
