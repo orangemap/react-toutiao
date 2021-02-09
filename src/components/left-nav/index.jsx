68d52b24dd0cc234dc6bf244776f03d31565514f
xnwangjianwen3 <wangjianwen777@163.com>
2021-02-09 17:53:31 +0800

    no message

---
+++ src/components/left-nav/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./index.less";
+class leftNav extends Component {
+    render() {
+        return (
+            <div className="left-nav">
+                leftNav
+            </div>
+        );
+    }
+}
+
+export default leftNav;
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import {Redirect} from "react-router-dom";
-
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { Layout } from "antd";
+import LeftNav from "../../components/left-nav/index";
+import Header from "../../components/header/index";
+const { Footer, Sider, Content } = Layout;
 class admin extends Component {
-    render() {
-        const   user = localStorage.getItem("user")
-        if(!user){
-           return <Redirect to="/login"/>
-        }
-        return (
-            <div>
-                hello,{user}
-            </div>
-        );
+  render() {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      return <Redirect to="/login" />;
     }
+    return (
+      <Layout style={{ height: "100%" }}>
+        <Sider>
+          <LeftNav></LeftNav>
+        </Sider>
+        <Layout>
+          <Header></Header>
+          <Content style={{ background: "white" }}>Content</Content>
+          <Footer style={{textAlign:"center",color:"rgba(0,0,0,0.5)"}}>
+            使用谷哥浏览器，效果体验更佳
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 export default admin;
